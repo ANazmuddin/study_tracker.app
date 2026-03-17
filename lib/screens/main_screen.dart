1e4a13730bed6def708a554c188fc73f0c6015bd
ANazmuddin <anazmuddin072@gmail.com>
2026-03-17 16:37:11 +0700

    add navigation bar

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'dashboard_page.dart';
+import 'stats_page.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  // Daftar halaman yang bisa dibuka
+  final List<Widget> _pages = [
+    const DashboardPage(),
+    const StatsPage(),
+    const Center(child: Text("Profil - Segera Hadir", style: TextStyle(fontSize: 20))),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex], // Menampilkan halaman sesuai index
+
+      // Navigasi Bawah
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color(0xFF0F172A),
+        selectedItemColor: Colors.blueAccent,
+        unselectedItemColor: Colors.grey[600],
+        showSelectedLabels: true,
+        showUnselectedLabels: false,
+        elevation: 0,
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.bar_chart_rounded), label: "Statistik"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profil"),
+        ],
+      ),
+    );
+  }
+}
